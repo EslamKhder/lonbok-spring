34840cdbace00da9ceff3898299767481c7de43a
eslam khder <eslamkhder11@gmail.com>
2022-07-01 18:01:43 +0200

    @Builder lombok

--- src/main/java/com/spring/lombok/model/Student.java
+++ src/main/java/com/spring/lombok/model/Student.java
@@ -1,6 +1,7 @@
 package com.spring.lombok.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Student {
 
     private Long id;
