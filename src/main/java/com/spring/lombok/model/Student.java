ddbaef46f087f49c73736632e6ca73dd1dcf721e
eslam khder <eslamkhder11@gmail.com>
2022-07-01 20:23:17 +0200

    @EqualsAndHashCode lombok

--- src/main/java/com/spring/lombok/model/Student.java
+++ src/main/java/com/spring/lombok/model/Student.java
@@ -2,6 +2,7 @@ package com.spring.lombok.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @Builder
 @ToString
+@EqualsAndHashCode
 public class Student {
 
     private Long id;
@@ -22,4 +24,19 @@ public class Student {
     private String address;
 
     private String age;
+
+    /*@Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if(obj == null) return false;
+        if(this.getClass() != obj.getClass()) return false;
+        Student student = (Student)obj;
+        return this.id == student.getId() && this.getName() == student.getName()
+                && this.getAddress() == student.getAddress() && this.getAge() == student.getAge();
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id * name.hashCode() * age.hashCode() * address.hashCode());
+    }*/
 }
