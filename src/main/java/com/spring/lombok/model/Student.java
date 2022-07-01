a515df7c0540d40f9b775ce37aff41c8021efe1c
eslam khder <eslamkhder11@gmail.com>
2022-07-01 17:41:59 +0200

    @Setter @Getter lombok

--- src/main/java/com/spring/lombok/model/Student.java
+++ src/main/java/com/spring/lombok/model/Student.java
@@ -1,5 +1,10 @@
 package com.spring.lombok.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Student {
 
     private Long id;
@@ -9,36 +14,4 @@ public class Student {
     private String address;
 
     private String age;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
 }
